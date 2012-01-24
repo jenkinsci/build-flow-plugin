ff5a99ec14d53dc1a57effe7b68614813c3a8cbd
Nicolas De Loof <nicolas.deloof@gmail.com>
2012-01-24 15:26:19 +0100

    copyright relaxed to LGPL

--- src/main/java/com/cloudbees/plugins/flow/BuildFlowAction.java
+++ src/main/java/com/cloudbees/plugins/flow/BuildFlowAction.java
@@ -1,19 +1,20 @@
 /*
- Copyright (C) 2011 CloudBees Inc.
+ * Copyright (C) 2011 CloudBees Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU Affero General Public License
- as published by the Free Software Foundation; either version 3
- of the License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, see <http://www.gnu.org/licenses/>.
-*/
 package com.cloudbees.plugins.flow;
 
 import hudson.model.BuildBadgeAction;
