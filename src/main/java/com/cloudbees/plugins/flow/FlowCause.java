32c80c0fa65e3a7df6be692a92667b318697b984
Nicolas De Loof <nicolas.deloof@gmail.com>
2012-09-12 11:46:23 +0200

    fix a NullPointer to render FlowCause after jenkins restart

--- src/main/java/com/cloudbees/plugins/flow/FlowCause.java
+++ src/main/java/com/cloudbees/plugins/flow/FlowCause.java
@@ -1,20 +1,33 @@
 package com.cloudbees.plugins.flow;
 
 import hudson.model.Cause;
+import jenkins.model.Jenkins;
 
 /**
  * @author: <a hef="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
 public class FlowCause extends Cause {
 
-    private final transient FlowRun flowRun;
+    private transient FlowRun flowRun;
+
+    private final String cause;
 
     public FlowCause(FlowRun flowRun) {
         this.flowRun = flowRun;
+        this.cause = flowRun.getParent().getFullName() + "#" + flowRun.getNumber();
+    }
+
+    public FlowRun getFlowRun() {
+        if (this.flowRun == null) {
+            int i = cause.lastIndexOf('#');
+            BuildFlow flow = Jenkins.getInstance().getItemByFullName(cause.substring(0,i), BuildFlow.class);
+            flowRun = flow.getBuildByNumber(Integer.parseInt(cause.substring(i+1)));
+        }
+        return flowRun;
     }
 
     @Override
     public String getShortDescription() {
-        return "Started by build flow " + flowRun.toString();
+        return "Started by build flow " + cause;
     }
 }
