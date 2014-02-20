b8ca8a8db3cd77e06612359ceea70418dd4b4d72
Robin Jarry <robin.jarry@6wind.com>
2014-02-20 17:54:23 +0100

    add the possibility to read dsl from a file
    This allows to store the DSL under source control with all the benefits
    that this can bring. This feature is only available when the "Flow run
    needs a workspace" option is enabled.
    
    Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

--- src/main/java/com/cloudbees/plugins/flow/BuildFlow.java
+++ src/main/java/com/cloudbees/plugins/flow/BuildFlow.java
@@ -64,6 +64,7 @@ public class BuildFlow extends Project<BuildFlow, FlowRun> implements TopLevelIt
     private final FlowIcon icon = new FlowIcon();
 
     private String dsl;
+    private String dslFile;
 
     private boolean buildNeedsWorkspace;
 
@@ -88,6 +89,14 @@ public class BuildFlow extends Project<BuildFlow, FlowRun> implements TopLevelIt
         this.buildNeedsWorkspace = buildNeedsWorkspace;
     }
 
+    public String getDslFile() {
+        return dslFile;
+    }
+
+    public void setDslFile(String dslFile) {
+        this.dslFile = dslFile;
+    }
+
     @Override
     protected void submit(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException, FormException {
         super.submit(req, rsp);
@@ -95,6 +104,7 @@ public class BuildFlow extends Project<BuildFlow, FlowRun> implements TopLevelIt
         this.buildNeedsWorkspace = json.getBoolean("buildNeedsWorkspace");
         if (Jenkins.getInstance().hasPermission(Jenkins.RUN_SCRIPTS)) {
             this.dsl = json.getString("dsl");
+            this.dslFile = Util.fixEmptyAndTrim(json.getString("dslFile"));
         }
     }
 
